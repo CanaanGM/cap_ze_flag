a482fde0b8d5bd1d41330ee874a5b28829264578
CanaanGM <reakak1@gmail.com>
2022-11-19 03:06:30 +0300

    why did it change ??

--- basic_mod_1.py
+++ basic_mod_1.py
@@ -27,11 +27,8 @@ def get_decoded_message(decoded_items: list) -> str:
 if __name__ == "__main__":
     basic_mod_1_message =\
     "202 137 390 235 114 369 198 110 350 396 390 383 225 258 38 291 75 324 401 142 288 397".split(" ")
-    basic_mod_2_message=\
-        "104 290 356 313 262 337 354 229 146 297 118 373 221 359 338 321 288 79 214 277 131 190 377".split(" ")
 
     coded_message_1 = [ f"{int(code) % 37}" for code in basic_mod_1_message]
-    coded_message_2 = [ f"{pow( int(code), -1 , 41)}" for code in basic_mod_2_message]
     
     alphas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789' # this should be with the digits according to https://www.dcode.fr/base-36-cipher
     cypher_alpha = {i:x for i,x in zip (range(36), alphas)}
